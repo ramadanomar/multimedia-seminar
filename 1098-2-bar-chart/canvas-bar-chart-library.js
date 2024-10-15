319930ced83648177998bf92f271a51fd94865f8
Ramadan Omar <55840806+ramadanomar@users.noreply.github.com>
2024-10-15 14:52:28 +0300

    Seminar-3 (#1)
    * add border outline to bar charts
    
    * tema2: datalabel under bar
    
    * made form submit the data to barchart
    
    * lambda function for event submit

--- 1098-2-bar-chart/canvas-bar-chart-library.js
+++ 1098-2-bar-chart/canvas-bar-chart-library.js
@@ -1,41 +1,51 @@
 class BarChart {
-    
-    /**
-     * The canvas on which the bar chart will be displayed
-    */
-    #canvas;
+  /**
+   * The canvas on which the bar chart will be displayed
+   */
+  #canvas;
 
-    /**
-     * Construts a new instance
-     * @param {HTMLCanvasElement} canvas 
-     */
-    constructor(canvas){
-        this.#canvas = canvas;
+  /**
+   * Construts a new instance
+   * @param {HTMLCanvasElement} canvas
+   */
+  constructor(canvas) {
+    this.#canvas = canvas;
+  }
+
+  /**
+   * Draws the bar chart
+   * @param {Array<number>} values
+   */
+  draw(values) {
+    const context = this.#canvas.getContext("2d");
+    context.fillStyle = "#DEDEDE";
+    context.fillRect(0, 0, this.#canvas.width, this.#canvas.height);
+
+    const maxValue = Math.max(...values);
+    const f = this.#canvas.height / maxValue;
+
+    const barWidth = this.#canvas.width / values.length;
+
+    for (let i = 0; i < values.length; i++) {
+      const barX = i * barWidth;
+      context.fillStyle = "red"; // #FF0000
+      context.strokeStyle = "#5e0808";
+      context.lineWidth = 2; // grosimea liniei
+
+      // inmultim cu 0.9 ca sa fie lungimea mai mica (90% din canvas height)
+      const barHeight = values[i] * f * 0.9;
+
+      const barY = this.#canvas.height - barHeight;
+
+      context.fillRect(barX + barWidth / 4, barY, barWidth / 2, barHeight);
+
+      // Barchart outline
+      context.strokeRect(barX + barWidth / 4, barY, barWidth / 2, barHeight);
+
+      context.fillStyle = "black";
+      context.font = "12px Arial";
+      context.textAlign = "center";
+      context.fillText(values[i], barX + barWidth / 2, this.#canvas.height - 5);
     }
-
-    /**
-     * Draws the bar chart
-     * @param {Array<number>} values 
-     */
-    draw(values){
-        const context = this.#canvas.getContext('2d');
-        context.fillStyle = '#DEDEDE';
-        context.fillRect(0, 0, this.#canvas.width, this.#canvas.height);
-
-        const maxValue = Math.max(...values);
-        const f = this.#canvas.height / maxValue;
-
-        const barWidth = this.#canvas.width / values.length;
-
-        context.fillStyle = 'red'; // #FF0000
-        for(let i = 0; i < values.length; i++){
-            const barX = i * barWidth;
-
-            const barHeight = values[i] * f;
-
-            const barY = 0;
-
-            context.fillRect(barX, barY, barWidth, barHeight);
-        }
-    }
+  }
 }
